7d30179a340cf3cf07b72d8928443b6ca52ce78c
noahmac <116388091+noahmcallister04@users.noreply.github.com>
2025-10-31 16:19:10 -0500

    Added time series, date, and error views

--- view.py
+++ view.py
@@ -26,3 +26,34 @@ class ConsoleUI:
                     print("Invalid choice. Please enter 1 or 2.")
             except ValueError:
                 print("Invalid choice. Please enter 1 or 2.")
+ @staticmethod
+    def get_time_series():
+        print("\nSelect the Time Series of the chart you want to Generate")
+        print("-" * 10)
+        print("1. Intraday")
+        print("2. Daily")
+        print("3. Weekly")
+        print("4. Monthly")
+
+        while True:
+            try:
+                choice = int(input("\nEnter time series option (1, 2, 3, 4): "))
+                if choice in [1, 2, 3, 4]:
+                    return choice
+                else:
+                    print("Invalid choice. Please enter 1, 2, 3, or 4.")
+            except ValueError:
+                print("Invalid input. Please enter a number (1-4).")
+
+    @staticmethod
+    def get_date(prompt):
+        date_str = input(f"\n{prompt}").strip()
+        return date_str
+    
+    @staticmethod
+    def display_error(message):
+        print(f"\nError: {message}")
+
+    @staticmethod
+    def display_success(message):
+        print(f"n{message}")
